b763e62c5d78c6bbc7349329854f3956dcc34aa5
csteinmetz1 <csteinmetz1@gmail.com>
2018-07-15 22:33:05 -0500

    update to training script

--- train.py
+++ train.py
@@ -1,12 +1,13 @@
-from util import load_spectrograms, GenerateIRs
+import pickle
+import datetime
+from util import load_spectrograms
 from model import build_spectral_ae
-from keras.callbacks import LambdaCallback
 
 # hyperparameters
-epochs = 1
-batch_size = 10
-latent_dim = 2
-n_filters = [32, 128, 256, 1024]
+epochs = 25
+batch_size = 1
+latent_dim = 1024
+n_filters = [64, 128, 256, 1024]
 
 # input
 input_shape = (513, 256, 1)
@@ -20,14 +21,18 @@ e, d, ae = build_spectral_ae(input_shape=input_shape,
 # load the data
 x_train, x_test = load_spectrograms('spectrograms', n_samples=10)
 
-# setup callback to generate IR samples
-#audio_callback = GenerateIRs(15, rate, batch_size, latent_dim, decoder)
+start_time = datetime.datetime.today()
 
 # train the thing
-ae.fit(x=x_train, y=x_train,
-       shuffle=True,
-       epochs=epochs,
-       batch_size=batch_size,
-       validation_data=(x_test, x_test))
+history = ae.fit(x=x_train, y=x_train,
+                shuffle=True,
+                epochs=epochs,
+                batch_size=batch_size,
+                validation_data=(x_test, x_test))
+
+end_time = datetime.datetime.today()
+
+print(f"Completed {epochs} in {end_time-start_time}.")
+# generate report here
 
 d.save("models/decoder.hdf5")
